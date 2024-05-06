f0107395965cef32f168c1f8f05a7e506e3b2720
Marr Raimund R. Salcedo <marr_salcedo@dlsu.edu.ph>
2024-05-06 10:42:53 +0800

    Updated the Admin Check and Prompt Customization for the legacy Windows PowerShell profile

--- WindowsPwsh/Microsoft.PowerShell_profile.ps1
+++ WindowsPwsh/Microsoft.PowerShell_profile.ps1
@@ -45,7 +45,7 @@ function prompt {
     if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
 }
 $adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
-$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
+$Host.UI.RawUI.WindowTitle = "Windows PowerShell {0}$adminSuffix"
 
 # Utility Functions
 function Test-CommandExists {
